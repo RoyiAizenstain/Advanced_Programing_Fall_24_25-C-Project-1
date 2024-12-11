3324ee1e4edf8d0c483623979630657250107a88
Royi <royi.aizen@gmail.com>
2024-12-11 19:55:12 +0200

    first

---
+++ Jerry.c
@@ -0,0 +1,3 @@
+// Created: 2018-10-10 15:45:00
+#include <stdio.h>
+#include "Jerry.h"
---
+++ Jerry.h
@@ -0,0 +1,8 @@
+//
+// Created by home on 12/11/2024.
+//
+
+#ifndef JERRY_H
+#define JERRY_H
+
+#endif //JERRY_H
