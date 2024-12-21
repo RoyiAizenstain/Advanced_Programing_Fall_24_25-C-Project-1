9321cb43d8744cba61ba44e9eb4de3c0621623c4
Royi <Royi.aizen@gmail.com>
2024-12-21 13:44:59 +0200

    Done for now.

---
+++ cmake-build-debug-wsl-1/CMakeFiles/Advanced_Programing_Fall_24_25.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/u2/CLionProjects/new/Jerry.c" "CMakeFiles/Advanced_Programing_Fall_24_25.dir/Jerry.c.o" "gcc" "CMakeFiles/Advanced_Programing_Fall_24_25.dir/Jerry.c.o.d"
+  "/mnt/c/Users/u2/CLionProjects/new/ManageJerries.c" "CMakeFiles/Advanced_Programing_Fall_24_25.dir/ManageJerries.c.o" "gcc" "CMakeFiles/Advanced_Programing_Fall_24_25.dir/ManageJerries.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug-wsl/CMakeFiles/Advanced_Programing_Fall_24_25.dir/compiler_depend.ts
@@ -0,0 +1,2 @@
+# CMAKE generated file: DO NOT EDIT!
+# Timestamp file for compiler generated dependencies management for Advanced_Programing_Fall_24_25.
